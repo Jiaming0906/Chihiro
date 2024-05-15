f391a2b6e34d9136fe346914a196fef13494a60e
Yu Jiaming <yuey9012@gmail.com>
2024-05-15 14:21:44 +0800

    Update filp.js and README.md

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-Easy to use Discord Bot.
-Run `node .\index.js` to run bot
+### Discord Bot created with Discord.js API
+
+First create *.env* file in your folder in the format:
+
+DISCORD_TOKEN=123456789
+CLIENT_ID=123456789
+
+Please change "123456789" accordingly.
+
+1. Run `node .\deploy-commands.js`
+2. Run `node .\index.js`
+
+Bot should be online.
--- commands/flip.js
+++ commands/flip.js
@@ -8,12 +8,38 @@ module.exports = {
         .setName('flip')
         .setDescription('Flips a coin'),
     async execute(interaction) {
+        //give heads and tails to teams 1 and 2
+        let team1 = "Heads";
+        let team2 = "Tails";
+        const test1 = Math.floor(Math.random() * 2);
+
+        if (test1) {
+            team1 = "Tails";
+            team2 = "Heads";
+        };
+
+        // console.log(test1);
+        
+        //Coin flip
         const n = Math.random();
+        let coinFlip = "Tails";
+
+        // console.log(n);
 
         if (n < 0.5){
-            return interaction.reply("Heads");
-        } else {
-            return interaction.reply("Tails");
-        }
+            coinFlip = "Heads";
+        };
+
+        //wins
+        let winTeam = "**Team 1** wins coinflip.\n∴ Team 1: Pick Map\n∴ Team 2: Ban Map, Pick Sides";
+        if (coinFlip === team2) {
+            winTeam = "**Team 2** wins coinflip.\n∴ Team 2: Pick Map\n∴ Team 1: Ban Map, Pick Sides";
+        };
+
+        //return team and coin flip results
+        const results = `Team 1: ${team1}\nTeam 2: ${team2}\n\nCoin Flip: **${coinFlip}** (n = ${n.toFixed(3)})¹\n¹*n = [0, 1); Heads when 0 ≤ n < 0.5; Tails when 0.5 ≤ n < 1*\n\n${winTeam}`
+
+        await interaction.reply({ content: `${results}` });
+        return;
     }
 };
